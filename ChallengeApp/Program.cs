43dcf43e91c951dec378a47dae0697f95294f211
RadekChojecki87 <radekch87xd@gmail.com>
2023-02-21 00:22:25 +0100

    ChallengeAppp

--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -1,1 +1,20 @@
-﻿Console.WriteLine("Hello,Radek!");
+﻿var name = "Karolina";
+bool female = true;
+var age = 36;
+
+if (female && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Karolina" && age == 36)
+{
+    Console.WriteLine("Karolina, 36 lat");
+}
+else if (!female && age < 20)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+else
+{
+    Console.WriteLine("Ktoś inny");
+}
