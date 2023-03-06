982be05abd7039820991c44563099ca4c4ed578d
RadekChojecki87 <radekch87xd@gmail.com>
2023-03-06 15:41:55 +0100

    TypeTests

--- ChallengeApp/User.cs
+++ ChallengeApp/User.cs
@@ -3,6 +3,12 @@
     public class User
     {
         private List<int> score = new List<int>();
+        public User(string login)
+        {
+            this.Login = login;
+        }
+
+
         public User(string login, string password)
         {
             this.Login = login;
